df2a2ec91f67d7bbba359dafa12035d7189dacca
ForMyke <mgomezl1801@alumno.ipn.mx>
2024-06-17 14:40:03 -0600

    cambios a la base

--- conectarABase/conexion.php
+++ conectarABase/conexion.php
@@ -8,12 +8,4 @@ $port = 3307;
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-else{
-    echo "conexion exitosa";
-}
 ?>
-
