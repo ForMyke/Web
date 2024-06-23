0a3b1d77df91bf76c1f22f3a9a9908cf62db260b
ForMyke <mgomezl1801@alumno.ipn.mx>
2024-06-23 00:57:46 -0600

    chat bot

--- src/components/AcercaDe.js
+++ src/components/AcercaDe.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/acercaDe.css";
 import ThreeFigure from "../Animaciones/ThreeFigure";
 import ThreeFigureExpanded from "../Animaciones/ThreeFigureExpanded";
 
 const AcercaDe = () => {
+  useEffect(() => {
+    const animateText = async (selector, speed = 50) => {
+      const elements = document.querySelectorAll(selector);
+      for (const element of elements) {
+        const text = element.getAttribute("data-texto");
+        element.textContent = "";
+        let i = 0;
+
+        await new Promise((resolve) => {
+          const interval = setInterval(() => {
+            if (i < text.length) {
+              element.textContent += text.charAt(i);
+              i++;
+            } else {
+              clearInterval(interval);
+              resolve();
+            }
+          }, speed);
+        });
+      }
+    };
+
+    const runAnimations = async () => {
+      await animateText(".animate-text-primary", 300);
+      await animateText(".animate-text-secondary", 400);
+      await animateText(".animate-text-three", 250);
+      await animateText(".animate-text-four", 250);
+      await animateText(".animate-text-five", 250);
+    };
+
+    runAnimations();
+  }, []);
+
   return (
     <div className="acerca-de-container black">
       <div
@@ -12,9 +45,10 @@ const AcercaDe = () => {
         style={{ height: "100vh" }}
       >
         <ThreeFigureExpanded color={0xffffff} />
-        <h1 className="centered-text text-white position-absolute">
-          Bienvenidos a Xclusive Store
-        </h1>
+        <h1
+          className="centered-text text-white position-absolute animate-text-primary"
+          data-texto="Bienvenidos a Xclusive Store"
+        ></h1>
       </div>
 
       <div className="container mt-5 text-white">
@@ -23,9 +57,12 @@ const AcercaDe = () => {
             <ThreeFigure color={0xffffff} />
           </div>
           <div className="col-lg-6 col-md-12 d-flex flex-column justify-content-center">
-            <h1 className="display-1">Innovación y Tecnología</h1>
-            <p className="lead">
-              En Xclusive Store, nos apasiona ofrecerte lo último en tecnología
+            <h1
+              className="display-1 animate-text-secondary"
+              data-texto="Innovación y Tecnología"
+            ></h1>
+            <p className="lead ">
+              "En Xclusive Store, nos apasiona ofrecerte lo último en tecnología
               y productos innovadores. Nuestra misión es brindarte una
               experiencia de compra única y exclusiva.
             </p>
@@ -33,7 +70,7 @@ const AcercaDe = () => {
         </div>
         <div className="row hero-image-text">
           <div className="col-12">
-            <h2>Quiénes Somos</h2>
+            <h3 className="animate-text-three" data-texto="Quiénes Somos"></h3>
             <p>
               Xclusive Store es más que una tienda, es un destino donde la
               tecnología y la innovación se encuentran. Desde nuestra fundación,
@@ -45,13 +82,19 @@ const AcercaDe = () => {
             </p>
           </div>
         </div>
-        <h1 className="text-center my-4">Nuestras Iniciativas</h1>
+        <h1
+          className="text-center my-4 animate-text-four"
+          data-texto="Nuestras Iniciativas"
+        ></h1>
         <div className="row initiative-section mb-4">
           <div className="col-lg-6 col-md-12 d-flex justify-content-center align-items-center">
             <ThreeFigure color={0xffffff} />
           </div>
           <div className="col-lg-6 col-md-12 d-flex flex-column justify-content-center">
-            <h2>Fomentando el Futuro Activo</h2>
+            <h2
+              className="animate-text-five"
+              data-texto="Fomentando el Futuro Activo"
+            ></h2>
             <p>
               En Xclusive Store, creemos en la importancia de un futuro activo y
               saludable. Por eso, invertimos en programas y actividades que
--- src/components/ServicioCliente.js
+++ src/components/ServicioCliente.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Button } from "react-bootstrap";
 import { GoogleGenerativeAI } from "@google/generative-ai";
@@ -7,36 +7,93 @@ import "../css/ServicioCliente.css";
 const API_KEY = "AIzaSyCiKobTx4HWZZI_ldoxP0PTUmVIl8akgcw";
 const genAI = new GoogleGenerativeAI(API_KEY);
 
+const permittedQuestions = {
+  Hola: "¡Hola! ¿En qué puedo ayudarte?",
+  "Que es Xclusive Store":
+    "Xclusive Store es una tienda en la cual lo más importante eres tú.",
+  Ubicacion:
+    "Estamos ubicados en Juan de Dios Batiz 711, 07700 Gustavo A. Madero, Mexico City.",
+  "¿Cuál es el estado de mi pedido?":
+    "Para conocer el estado de tu pedido, por favor ingresa a tu cuenta y revisa la sección de 'Mis Pedidos'.",
+  "¿Cómo puedo devolver un producto?":
+    "Para devolver un producto, por favor visita nuestra sección de 'Devoluciones' y sigue las instrucciones.",
+  "¿Cuál es la política de garantía?":
+    "Nuestra política de garantía cubre defectos de fábrica por un periodo de un año desde la fecha de compra.",
+  "¿Cómo puedo cambiar mi dirección de envío?":
+    "Puedes cambiar tu dirección de envío desde la sección 'Mi Cuenta' en nuestro sitio web.",
+  "¿Cómo puedo contactar al soporte técnico?":
+    "Puedes contactar al soporte técnico enviando un correo a soporte@xclusivestore.com o llamando al 555-1234.",
+};
+
 const ServicioCliente = () => {
   const [inputText, setInputText] = useState("");
-  const [responseText, setResponseText] = useState("");
+  const [messages, setMessages] = useState([]);
+  const chatBoxRef = useRef(null);
 
   const handleInputChange = (e) => {
     setInputText(e.target.value);
   };
 
   const handleGenerateClick = async () => {
-    const model = genAI.getGenerativeModel({
-      model: "gemini-1.5-pro-latest",
-    });
-    const prompt = inputText;
+    const question = inputText.toLowerCase();
 
-    try {
-      const result = await model.generateContent(prompt);
-      const response = await result.response;
-      setResponseText(await response.text());
-    } catch (error) {
-      console.error("Error no jala :", error);
+    // Agregar mensaje del usuario a los mensajes
+    setMessages((prevMessages) => [
+      { sender: "user", text: inputText },
+      ...prevMessages,
+    ]);
+
+    // Buscar respuesta predefinida
+    const predefinedResponse = Object.keys(permittedQuestions).find((key) =>
+      question.includes(key.toLowerCase())
+    );
+
+    if (predefinedResponse) {
+      setMessages((prevMessages) => [
+        { sender: "bot", text: permittedQuestions[predefinedResponse] },
+        ...prevMessages,
+      ]);
+      setInputText("");
+      return;
     }
+
+    // Si no es una pregunta permitida, agregar mensaje de fase beta
+    setMessages((prevMessages) => [
+      {
+        sender: "bot",
+        text: "Lo siento, no puedo responder a esa pregunta en este momento. El servicio está en fase beta.",
+      },
+      ...prevMessages,
+    ]);
+
+    setInputText("");
   };
+
+  useEffect(() => {
+    if (chatBoxRef.current) {
+      chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   return (
     <div className="container d-flex flex-column align-items-center justify-content-center vh-100">
-      <div className=" p-4 shadow">
+      <div className="card w-100 p-4 shadow chat-container">
         <div className="logo mb-4 text-center">
           <img src="./img/logo.png" alt="Xclusive Store" className="logo" />
         </div>
         <h2 className="text-center">Soporte Técnico</h2>
+        <div className="chat-box mb-3 p-3 border rounded" ref={chatBoxRef}>
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message ${
+                message.sender === "user" ? "user-message" : "bot-message"
+              } mb-2`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
         <Form onSubmit={(e) => e.preventDefault()} className="w-100">
           <Form.Group controlId="inputText" className="mb-3">
             <Form.Label>Escribe tu consulta</Form.Label>
@@ -53,14 +110,9 @@ const ServicioCliente = () => {
             className="w-100"
             onClick={handleGenerateClick}
           >
-            Generar
+            Enviar
           </Button>
         </Form>
-        {responseText && (
-          <p id="responseText" className="mt-4 p-3 bg-light border rounded">
-            {responseText}
-          </p>
-        )}
       </div>
     </div>
   );
