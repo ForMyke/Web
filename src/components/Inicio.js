17c1687d94f2850f7f086430ef0eba538cbf7459
ForMyke <mgomezl1801@alumno.ipn.mx>
2024-06-22 19:24:49 -0600

    Rama Front Terminada

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -75,7 +75,7 @@ const Inicio = () => {
   return (
     <div className="inicio-container">
       <Alert variant="light" className="text-center mb-0">
-        Unete y se parte de la familia Xclusive Store
+        Unete ahora y se parte de la familia Xclusive Store
       </Alert>
       <div className="flash-sale bg-dark text-white text-center py-2">
         <h2 className="mb-0">Descuentos en todas nuestras áreas</h2>
